a44afd854bfc5d2161feb4c371c6be1d1606e1d7
BirchJD <jason-birch@hotmail.co.uk>
2017-05-12 13:22:12 +0100

    Start of refining PIC test code and additional PIC device support.

--- TEST_ASM/PIC12F675/PIC12F675.asm
+++ TEST_ASM/PIC12F675/PIC12F675.asm
@@ -9,8 +9,8 @@
 ;/*************/
 ;/* Constants */
 ;/*************/
-GPIO_LED          EQU      0x04                 ; GPIO pin allocated for driving an LED.
-GPIO_SWITCH       EQU      0x08                 ; GPIO pin allocated for driving an LED.
+GPIO_LED          EQU      (1 << GP5)           ; GPIO pin allocated for driving an LED.
+GPIO_SWITCH       EQU      (1 << GP4)           ; GPIO pin allocated for sensing a switch press.
 
 DO_FLASH_COUNT    EQU      0x06                 ; Number of times to invert LED on button press.
 
@@ -32,8 +32,8 @@ ENDC
 ;/**********************************/
                   ORG      0x0000
 
-                  CLRF     STATUS               ; Clear the status register and select register bank 0
-                  BCF      STATUS, RP0          ; Select Register bank 0
+                  BSF      STATUS, RP0          ; Select Register bank 1
+                  CLRF     ANSEL                ; Switch off A/D pins, all pins digital.
                   GOTO     INIT
 
 
@@ -43,96 +43,83 @@ ENDC
 ;/*************************************/
                   ORG      0x0004
 
-                  GOTO     INT_HANDLE
-
-
-
 INT_HANDLE        BCF      STATUS, RP0          ; Select Register bank 0
+
                   BTFSS    PIR1, TMR1IF         ; Did a TIMER1 interupt trigger?
-                  GOTO     INT_TIMER1_END
-                  MOVFW    FLASH_COUNT          ; Is a flashing process active?
-                  ANDLW    0xFF
+                  GOTO     INT_GPI
+                  MOVF     FLASH_COUNT, F       ; Is a flashing process active?
                   BTFSC    STATUS, Z
                   GOTO     INT_TIMER1_END
-                  MOVLW    ~GPIO2               ; Invert LED.
-                  XORWF    GPIO
+                  COMF     GPIO                 ; Invert LED.
                   DECF     FLASH_COUNT          ; Reduce flash count.
+INT_TIMER1_END    BCF      PIR1, TMR1IF
 
-INT_TIMER1_END:   BCF      PIR1, TMR1IF
-
-                  BTFSS    INTCON, GPIF         ; Did an input pin interupt trigger?
-                  GOTO     INT_GPI_END
-                  MOVFW    FLASH_COUNT          ; Is a flashing process active?
-                  ANDLW    0xFF
+INT_GPI           BTFSS    INTCON, GPIF         ; Did an input pin interupt trigger?
+                  GOTO     INT_END
+                  MOVF     FLASH_COUNT, F       ; Is a flashing process active?
                   BTFSS    STATUS, Z
                   GOTO     INT_GPI_END
-                  MOVLW    0x00                 ; Configure full TIMER1 period.
-                  MOVWF    TMR1L
-                  MOVLW    0x00
-                  MOVWF    TMR1H
+                  CLRF     TMR1L                ; Configure full TIMER1 period.
+                  CLRF     TMR1H
                   MOVLW    DO_FLASH_COUNT       ; Set flash LED count.
                   MOVWF    FLASH_COUNT
+INT_GPI_END       BCF      INTCON, GPIF
 
-INT_GPI_END:      BCF      INTCON, GPIF
-                  RETFIE
+INT_END           RETFIE
 
 
 
 ;/*******************************/
 ;/* Initialise microcontroller. */
 ;/*******************************/
-                  ORG      0x0100
-
-INIT:             CLRF     GPIO
-                  CLRF     PIR1
-                  MOVLW    0x07                 ; All GPIO as digital IO.
-                  MOVWF    CMCON
-                  MOVLW    0x3D                 ; Configure Timer1.
-                  MOVWF    T1CON
-                  BSF      STATUS, RP0          ; Select Register bank 1
-                  CLRF     WPU
-                  CLRF     ANSEL
-                  MOVLW    0x0F                 ; Prescale watchdog timer.
+INIT              MOVLW    0x0F                 ; Prescale watchdog timer.
                   MOVWF    OPTION_REG
                   MOVLW    ~GPIO_LED            ; All GPIO as an input except LED GPIO.
                   MOVWF    TRISIO
-                  MOVLW    0xC8                 ; Configure GPIO input.
-                  MOVWF    INTCON
-                  MOVLW    GPIO_SWITCH          ; Configure GPIO input.
-                  MOVWF    IOC
-                  MOVLW    0x01                 ; Configure Timer1.
+                  MOVLW    GPIO_SWITCH
+                  MOVWF    WPU                  ; Weak pull up on switch.
+                  MOVWF    IOC                  ; Interupt on change of switch state.
+                  MOVLW    (1 << TMR1IE)        ; Intrupt on Timer1 overflow.
                   MOVWF    PIE1
 
                   BCF      STATUS, RP0          ; Select Register bank 0
+                  MOVLW    0x07                 ; Switch comparitor off.
+                  MOVWF    CMCON
+                  CLRF     GPIO                 ; Clear GPIO port state.
+                  MOVLW    (1 << TMR1ON)|(1 << NOT_T1SYNC)|(1 << T1CKPS0)|(1 << T1CKPS1)
+                  MOVWF    T1CON                ; Configure Timer1.
+                  MOVLW    (1 << GIE)|(1 << PEIE)|(1 << GPIE)
+                  MOVWF    INTCON               ; Enable interupts.
+                  CLRF     PIR1                 ; Clear interupt triggered flags.
+
                   CLRF     FLASH_COUNT          ; Clear the flash count RAM register.
 
 LOOP              SLEEP                         ; Low power mode when inactive.
 ACTIVE_LOOP       CLRWDT                        ; Tell the CPU the program is still running.
-                  MOVFW    FLASH_COUNT          ; Is a flashing process active?
-                  ANDLW    0xFF
+                  MOVF     FLASH_COUNT, F       ; Is a flashing process active?
                   BTFSS    STATUS, Z
-                  GOTO     ACTIVE_LOOP
-                  GOTO     LOOP                 ; Infinite main loop, waiting for interupts.
+                  GOTO     ACTIVE_LOOP          ; Keep processor awake to receive timer interupts.
+                  GOTO     LOOP                 ; Infinite main loop.
 
 
 
 ;/*********************************/
 ;/* Write data to data area test. */
 ;/*********************************/
-                  ORG      0x2000 ; Area for PIC ID  - compiler will complain.
+                  ORG      0x2000               ; Area for PIC USER ID - compiler will complain.
 
                   DE       0x55, 0x55, 0x55, 0x55
 
 
-                  ORG      0x2100
+                  ORG      0x2100               ; EEPROM Area.
 
                   DE       0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF
 
 
-                  ORG      0x2110
+                  ORG      0x2110               ; EEPROM Area.
 
-                  DE       0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07
-                  DE       0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F
+                  DE       0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07
+                  DE       0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
 
 
                   END
